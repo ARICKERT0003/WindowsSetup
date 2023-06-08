199bc5804b9da8c5692e5e6e7065b915ec8e4534
Alex Rickert <ARickert0003@gmail.com>
2023-06-08 15:28:07 -0400

    Add quartus and fix some directories

--- install.ps1
+++ install.ps1
@@ -15,7 +15,7 @@ $local_install = '~\AppData\Local'
 mkdir ~/Projects
 
 # ============================
-# Install Programs
+# Install Various Programs
 # ============================
 
 # Update Powershell to most recent
@@ -28,20 +28,49 @@ winget install -e --id Git.Git
 winget install -e --id Python.Python.3.0
 
 # Install nerdfonts ( prereq for starship idk why)
-mkdir $LOCAL_INSTALL\fonts
-git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts.git $LOCAL_INSTALL\fonts
+git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts.git $LOCAL_INSTALL/nerd-fonts
+cd $LOCAL_INSTALL/nerd-fonts
+git sparse-checkout add patched-fonts/JetBrainsMono
+cd ~/
 
+# Install Starship 
+# - Allows me to customize terminal and make it more efficient/comfortable to program in
+# - Website https://starship.rs/ 
+winget install starship
 
 # ============================
-# Text Editor
+# Install Text Editor
 # ============================
 
 # Install text editor (Vim)
 winget install -e --id vim.vim
 
 # Create directories for editor config files and packages
-mkdir ~/vimfiles ~/vimfiles/backup ~/vimfiles/pack ~/vimfiles/swap
+mkdir ~/vimfiles 
+mkdir ~/vimfiles/backup 
+mkdir ~/vimfiles/pack 
+mkdir ~/vimfiles/swap
 
 # NERDTree (file explorer)
-mkdir ~/vimfiles/NERDTree/start
+mkdir ~/vimfiles/pack/NERDTree/start
 git clone https://github.com/preservim/nerdtree ~/vimfiles/NERDTree/start/
+
+# ============================
+# Install Quartus (Big)
+# ============================
+
+# Install Quartus Lite 18.1
+#curl --output-dir ~/Downloads https://cdrdv2.intel.com/v1/dl/getContent/665990/666029?filename=Quartus-lite-18.1.0.625-windows.tar
+#tar -xvzf ~/Downloads/Quartus-lite-18.1.0.625-windows.tar -C ~/Downloads
+#start-process ~/Downloads/Quartus-lite-18.1.0.625-windows/setup.bat
+
+# Install Quartus Standard 18.1
+#curl --output-dir ~/Downloads https://cdrdv2.intel.com/v1/dl/getContent/665987/666013?filename=Quartus-18.1.0.625-windows-complete.tar
+#tar -xvzf ~/Downloads/Quartus-lite-18.1.0.625-windows-complete.tar -C ~/Downloads
+#start-process ~/Downloads/Quartus-lite-18.1.0.625-windows-complete/setup.bat
+
+# Install Quartus Pro 19.3
+#curl --output-dir ~/Downloads https://cdrdv2.intel.com/v1/dl/getContent/661657/661690?filename=Quartus-pro-19.3.0.222-windows-complete.tar
+#tar -xvzf ~/Downloads/Quartus-lite-18.1.0.625-windows-complete.tar -C ~/Downloads
+#start-process ~/Downloads/Quartus-lite-18.1.0.625-windows-complete/setup.bat
+
